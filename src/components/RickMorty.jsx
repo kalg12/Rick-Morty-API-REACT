61b13dd45e6e4726a0d4ffa19b13886b6850e853
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-01-25 09:06:29 -0600

    Hooks

--- src/components/RickMorty.jsx
+++ src/components/RickMorty.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 const RickMorty = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
   return (
     <>
       <h1>Rick and Morty</h1>
