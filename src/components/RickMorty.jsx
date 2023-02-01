0153bceb4291d72a3abd2bf0cdf439d7f5096dee
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-02-01 08:38:08 -0600

    API

--- src/components/RickMorty.jsx
+++ src/components/RickMorty.jsx
@@ -1,17 +1,29 @@
 import React, { useState, useEffect } from "react";
 
 const RickMorty = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character")
       .then((response) => response.json())
-      .then((data) => setData(data));
-  }, []);
+      .then((info) => setData(info) & console.log(data));
+  }, [data]);
   return (
     <>
       <h1>Rick and Morty</h1>
-      {console.log(data)}
+      <div className="card" style={{ width: "18rem" }}>
+        {/* <img src={data.results[0].image} className="card-img-top" alt="..." /> */}
+        <div className="card-body">
+          <h5 className="card-title">Title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
     </>
   );
 };
